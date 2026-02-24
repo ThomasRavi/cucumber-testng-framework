13e8b34f6154d8d698d862d2b79fb611f4f4c261
Thomas Ravi <thomasravi@gmail.com>
2026-02-24 15:09:42 +0530

    Added GitHub CI Workflow

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(
 features="src/test/resources/features",
 glue={"stepdefinitions","hooks","api"},
-tags	="@ui",
+tags	="@api",
 plugin={"pretty","html:target/report.html"}
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
